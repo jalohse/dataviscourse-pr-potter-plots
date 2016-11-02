6df4050db8ab6181d680122633cc776358b9cdd5
Jessica Lohse <jalohse62442@gmail.com>
2016-11-01 20:37:19 -0600

    Add template html and load data

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+(function(){
+    var instance = null;
+
+    function init() {
+        var spellChart = new SpellChart();
+
+
+
+        d3.json('data/books.json', function(error, data) {
+            bookData = data;
+            console.log(bookData);
+        })
+    }
+
+
+    function Main(){
+        if(instance  !== null){
+            throw new Error("Cannot instantiate more than one Class");
+        }
+    }
+
+
+    Main.getInstance = function(){
+        var self = this;
+        if(self.instance == null){
+            self.instance = new Main();
+
+            init();
+        }
+        return instance;
+    };
+
+    Main.getInstance();
+})();
