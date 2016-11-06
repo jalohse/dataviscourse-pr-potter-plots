5cec4ceadbd5864d46c7a9f56a264f39018915ce
Jessica Lohse <jessicaalohse@gmail.com>
2016-11-06 15:11:00 -0700

    Add tooltip for gross chart

--- js/gross.js
+++ js/gross.js
@@ -1,5 +1,3 @@
-
-
 function GrossChart(data) {
 
     var width = 900;
@@ -14,21 +12,21 @@ function GrossChart(data) {
 
     var properties = ["book_gross", "movie_gross"];
     data.forEach(function (d) {
-        if(d.book != " Deathly Hallows") {
+        if (d.book != " Deathly Hallows") {
             d.gross = properties.map(function (property) {
                 return {name: d.book, property: property, number: d[property], color: d.color};
             });
         }
     });
     var twoMovieProperties = ["book_gross", "movie1_gross", "movie2_gross"];
-    var dh = data[data.length -1];
+    var dh = data[data.length - 1];
     dh.gross = twoMovieProperties.map(function (property) {
         return {name: dh.book, property: property, number: dh[property], color: dh.color};
     });
 
 
     var maxGross = d3.max(data, function (d) {
-        return d3.max(d.gross, function(i){
+        return d3.max(d.gross, function (i) {
             return i.number;
         });
 
@@ -36,7 +34,7 @@ function GrossChart(data) {
 
     var xScale = d3.scaleBand()
         .domain(data.map(function (d) {
-            return d3.max(d.gross, function(i){
+            return d3.max(d.gross, function (i) {
                 return i.name;
             });
         })).range([leftOffset, width - 60])
@@ -59,17 +57,17 @@ function GrossChart(data) {
     var defs = grossChart.append("defs");
     defs.selectAll("pattern")
         .data(bookData).enter().append("pattern")
-        .attr('id',function (d) {
+        .attr('id', function (d) {
             return "pattern_" + d.color;
         })
         .attr("width", 80)
         .attr("height", 10)
-        .attr('patternUnits',"userSpaceOnUse")
+        .attr('patternUnits', "userSpaceOnUse")
         .append('rect')
         .attr("width", 80)
         .attr("height", 5)
         .attr("fill", function (d) {
-           return d.color;
+            return d.color;
         });
 
     var xAxis = d3.axisBottom();
@@ -88,13 +86,38 @@ function GrossChart(data) {
 
     var bookMovie = grossChart.selectAll(".group")
         .data(data).enter().append("g")
-        .attr("transform", function(d){
+        .attr("transform", function (d) {
             return "translate (" + xScale(d.book) + ",0)";
         });
 
+    tip = d3.tip().attr('class', 'd3-tip')
+        .direction('ne')
+        .offset(function () {
+            return [50,0];
+        })
+        .html(function (d) {
+            var text = "<p style='font-size: 20px; text-align: center; color: "
+            + d.color + ";'><b><i>" + d.name;
+            if(d.property.includes("movie1")){
+                text += " Part 1</i>: ";
+            } else if (d.property.includes("movie2")){
+                text += " Part 2</i>: ";
+            } else if(d.property.includes("movie")) {
+                text += "</i> movie: ";
+            } else {
+                text += "</i> book: ";
+            }
+            text += "$" + d.number + " million</b><p>";
+            return text;
+        });
+
+    grossChart.call(tip);
+
+
+
     var bars = bookMovie.append('g')
         .selectAll('rect');
-    var barData = bars.data(function(d){
+    var barData = bars.data(function (d) {
         return d.gross;
     });
     var newBars = barData
@@ -111,14 +134,14 @@ function GrossChart(data) {
     bars.exit().remove();
     bars = newBars.merge(bars);
     bars.attr('x', function (d) {
-        if(d.name == " Deathly Hallows"){
+        if (d.name == " Deathly Hallows") {
             return x3Scale(d.property);
         }
         return x2Scale(d.property);
     }).attr("y", function (d) {
         return yScale(d.number);
-    }).attr("width", function(d){
-        if(d.name == " Deathly Hallows"){
+    }).attr("width", function (d) {
+        if (d.name == " Deathly Hallows") {
             return x3Scale.bandwidth();
         }
         return x2Scale.bandwidth()
@@ -126,26 +149,29 @@ function GrossChart(data) {
         .attr("height", function (d) {
             return barHeight - yScale(d.number);
         }).style("fill", function (d) {
-        if(d.property.includes("movie")){
-            return "url(#pattern_"+ d.color + ")";
+        if (d.property.includes("movie")) {
+            return "url(#pattern_" + d.color + ")";
         } else {
             return d.color;
         }
-    }).attr("stroke", function(d){
-        if(d.property.includes("movie")){
+    }).attr("stroke", function (d) {
+        if (d.property.includes("movie")) {
             return d.color;
         }
-    });
+    }).on("mouseover", tip.show)
+        .on("mouseout", tip.hide);
 
     d3.select("g path").attr("d", "M40.5,6V0.5H890.5V6");
     var ticks = d3.selectAll("g.tick")._groups[0];
-    for(i = 0; i < ticks.length; i++){
-        if(ticks[i].getAttribute("transform") == "translate(778.0281690140844,0)"){
-            var newVal = 778.0281690140844 + xScale.bandwidth()/4 ;
+    for (i = 0; i < ticks.length; i++) {
+        if (ticks[i].getAttribute("transform") == "translate(778.0281690140844,0)") {
+            var newVal = 778.0281690140844 + xScale.bandwidth() / 4;
             ticks[i].setAttribute("transform", "translate(" + newVal + ", 0)");
         }
     }
 
 }
+
+
 
 
